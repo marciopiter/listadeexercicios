1a75053763fda1edc20b6cf696bc2343b37bf286
aluno <aluno@MSCGRESL04016.estacio.acad>
2017-09-14 21:42:01 -0300

    Exercicio MiniProva

---
+++ src/exercio01/Exercio01.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercio01;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercio01 {
+
+    //1.Explique o processo de compilação e interpretação do JAVA.
+
+ processo de compilação de uma linguagem  descrita intermediaria  em java 
+ processo de interpretação da linguagem intermediária durante a execução do programa,
+ consiste transforma os comandos da linguagem intermediária para linguagem de máquina.
+
+ // 2.	Para executar um programa Java deve ocorrer um processo 
+        que envolve compilação e interpretação. Quando se compila uma classe com extensão .java 
+         é gerado um arquivo com extensã Bytecode é o resultado de um processo semelhante 
+         ao dos compiladores de código-fonte       que não é imediatamente executável
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 3. Crie um código Java, versionado no git, com a definição das variáveis para:
+     int idade = 37;
+     String nome = "marcio";
+     double peso = 75;
+     char Tiposangue = 'O';
+     String aniversário = "14/03/1980";
+     int numSapato = 40;
+     String Casado = "nao"; 
+     
+     //4. (AULA 3 - Variáveis e tipos) O que sai na tela ao executarmos a linha, por que isso acontece?
+     System.out.println (.1+ .2 ==.3);
+     // resposta : na hora de converter  os lixos que restao nao batem 
+     
+    //4. (AULA 3 - Variáveis e tipos) O que sai na tela ao executarmos a linha, por que isso acontece?
+     
+     int a = 3;
+     int b = 1;
+     int c = b + 1;
+     int d = c > ++b ? (a - 3) : b;
+     int e = 4;
+ if( d > a && b + 1 < a || b > 1)
+     c += 7;
+if( -7 >= -c)
+    a -= 4;
+else
+  d *= 2;
+     e = ++d % 3;
+     b = d++ % 3;
+6.
+    }
+    
+   
+    
+            
+}
